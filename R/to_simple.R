78b56f3a2453fda21b3aa5c0804121c7f4e0e806
Giulio Valentino Dalla Riva￼ <me@gvdallariva.net>
2020-01-27 19:41:48 +1300

    removed ::: calls

--- R/to_simple.R
+++ R/to_simple.R
@@ -6,22 +6,28 @@
 #' @param remove_multiples whether to remove multiple edges
 #' @param remove_loops whether to remove loops
 #'
-#' @importFrom igraph simplify
+#' @importFrom igraph simplify edge_attr edge_attr<-
 #'
 #' @export
 #'
 to_simple <- function(graph, remove_multiples = TRUE, remove_loops = FALSE) {
-  edges <- as_tibble(graph, active = "edges")
-  graph <- tidygraph:::set_edge_attributes(graph, edges[, ".tidygraph_edge_index",
+  edges <- tidygraph::as_tibble(graph, active = "edges")
+  graph <- set_edge_attributes(graph, edges[, ".tidygraph_edge_index",
                                             drop = FALSE])
   edges$.tidygraph_edge_index <- NULL
-  simple <- as_tbl_graph(simplify(graph,
+  simple <- tidygraph::as_tbl_graph(simplify(graph,
                                   remove.multiple = remove_multiples,
                                   remove.loops = remove_loops,
                                   edge.attr.comb = list))
-  new_edges <- as_tibble(simple, active = "edges")
+  new_edges <- tidygraph::as_tibble(simple, active = "edges")
   new_edges$.orig_data <- lapply(new_edges$.tidygraph_edge_index,
                                  function(i) edges[i, , drop = FALSE])
-  simple <- tidygraph:::set_edge_attributes(simple, new_edges)
+  simple <- set_edge_attributes(simple, new_edges)
   list(simple = simple)
+}
+
+set_edge_attributes <- function(x, value) {
+  value <- value[, !names(value) %in% c("from", "to")]
+  edge_attr(x) <- as.list(value)
+  x
 }
